ae8c88fc36a4e11c506d2da5456a5d0754931c8a
Julioipl <127806278+Julioipl@users.noreply.github.com>
2024-04-08 20:23:37 -0600

    Delete Formulario/script.js

--- Formulario/script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const botonModoOscuro = document.getElementById('modoOscuroBtn');
-    const body = document.body;
-
-    botonModoOscuro.addEventListener('click', function() {
-        body.classList.toggle('dark-mode');
-    });
-});
